315d774e39f3cd648de8b1e235e90d9bcb00cc02
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-15 11:09:48 +0530

    Implemented Shopping Api Microservice and it is being consumed by shopping client microservice

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System.Collections.Generic;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
